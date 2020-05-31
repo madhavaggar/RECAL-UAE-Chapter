e3c9da95a1027c13c15bcfa65d528cd91293fa1d
nsi319 <sairam.nareniyer@gmail.com>
2020-05-31 19:29:03 +0530

    changed structure

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import './Constant/Constant.dart';
+import './Screen/HomePage.dart';
+import './Screen/ImageSplashScreen.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  runApp(new MaterialApp(
+    title: 'FluterSplashDemo',
+    debugShowCheckedModeBanner: false,
+    theme: new ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: new ImageSplashScreen(),
+    routes: <String, WidgetBuilder>{
+      HOME_SCREEN: (BuildContext context) => new HomeScreen(),
+      IMAGE_SPLASH: (BuildContext context) => new ImageSplashScreen()
+    },
+  ));
+}
+
