84e0131957728d2c6ee0c8ca903b6db86ab8d1b9
nsi319 <sairam.nareniyer@gmail.com>
2020-06-14 19:26:37 +0530

    uae chapter page

--- lib/UAEChapter/ChapterScreen.dart
+++ lib/UAEChapter/ChapterScreen.dart
@@ -1,5 +1,10 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'VisionandMission.dart';
+import 'Sponsors.dart';
+import 'CoreComm.dart';
 import '../Constant/ColorGlobal.dart';
 
 class ChapterScreen extends StatefulWidget {
@@ -10,6 +15,8 @@ class ChapterScreen extends StatefulWidget {
 class _ChapterScreenState extends State<ChapterScreen> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -19,11 +26,107 @@ class _ChapterScreenState extends State<ChapterScreen> {
             style: TextStyle(color: ColorGlobal.textColor),
           ),
         ),
-        body: Container(
-          child: Center(
-            child: Text("UAE Chapter"),
+
+        body: Flex(
+          direction: Axis.vertical,
+          children: [
+            Expanded(
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 15),
+                    width: width*0.7,
+                    height: width*0.3,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    decoration: new BoxDecoration(
+                      color: ColorGlobal.colorPrimaryDark,
+                      image: new DecorationImage(
+                        image: new AssetImage('assets/images/recal_logo.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                      borderRadius: BorderRadius.circular(width*0.1)
+                      ),
+                  ),
+      ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ListBody(
+                      children: <Widget>[
+                        Text(
+                          "RECAL",
+                          style: Theme.of(context).textTheme.headline,
+                          textAlign: TextAlign.center,
+                        ),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: Container(
+                                  padding: const EdgeInsets.only(top: 8, bottom: 8),
+                                  decoration: const BoxDecoration(
+                                    border: Border(
+                                      top: BorderSide(width: 0),
+                                      bottom: BorderSide(width: 0),
+                                    ),
+                                  ),
+                                  child: Text("REC's (NIT Trichy) Alumni Association (RECAL).",
+                                  style: TextStyle(fontSize: 15),),
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                Container(
+                  height: 200,
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.list,size: 30),
+                        title: Text('Vision and Mission'),
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.downToUp,
+                                duration: Duration(milliseconds: 300),
+                                child: VisionMission()),);
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.person,size: 30),
+                        title: Text('Core Committee'),
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.leftToRight,
+                                duration: Duration(milliseconds: 300),
+                                child: CoreComm()),);
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.attach_money,size: 30),
+                        title: Text('Sponsors'),
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.rightToLeft,
+                                duration: Duration(milliseconds: 300),
+                                child: Sponsors()),);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          ],
+        )
       ),
     );
   }
